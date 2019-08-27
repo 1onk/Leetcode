bcca551aaed0ea0c21d241bbb14730f36d0a2771
liujianglong <406061841@qq.com>
2019-08-27 18:47:00 +0800

    4

--- 0-99/4.py
+++ 0-99/4.py
@@ -11,3 +11,18 @@ class Solution:
         lo, hi = 0, 2 * n
 
         while lo <= hi:
+            c1 = (lo + hi) // 2
+            c2 = m + n - c1
+            Lmax1 = float('-inf') if c1 == 0 else nums1[(c1-1) // 2]
+            Rmin1 = float('inf') if c1 == 2 * n else nums1[c1 // 2]
+            Lmax2 = float('-inf') if c2 == 0 else nums2[(c2-1) // 2]
+            Rmin2 = float('inf') if c2 == 2 * m else nums2[c2 // 2]
+
+            if Lmax1 > Rmin2:
+                hi = c1 - 1
+            elif Lmax2 > Rmin1:
+                lo = c1 + 1
+            else:
+                break
+
+        return (max(Lmax1, Lmax2) + min(Rmin1, Rmin2)) / 2
